49b8c2216adb0591bf759512fedc611b7b5616f5
patrikpcm <patrikpcm@hotmail.com>
2023-10-09 16:11:27 -0300

    Day 3: Intro to Conditional Statements

---
+++ 30 Days of Code/intro_to_conditional_statements.cpp
@@ -0,0 +1,23 @@
+
+int main()
+{
+    string N_temp;
+    getline(cin, N_temp);
+
+    int N = stoi(ltrim(rtrim(N_temp)));
+    
+    if(N%2 == 0){
+        if(N>=2 && N<=5)
+            cout << "Not Weird";
+        
+        else if(N>=6 && N<=20)
+            cout << "Weird";
+        
+        else if (N > 20)
+            cout << "Not Weird";   
+    }
+    else
+        cout << "Weird";
+
+    return 0;
+}
